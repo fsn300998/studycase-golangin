4aab4d6e72d2a4920d42bb0c741a863a4388d04e
fsn300998 <fsn300998@gmail.com>
2024-01-08 19:44:10 +0700

    case 1

--- case1/Main.go
+++ case1/Main.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
+type identitas struct {
+	Nama   string
+	Umur   int
+	Alamat string
+}
+
+var data []identitas
+
 func main() {
-	fmt.Println("Hello World")
+	m1 := identitas{
+		Nama:   "Fajrin",
+		Umur:   25,
+		Alamat: "Tasikmalaya",
+	}
+	data = append(data, m1)
+	for _, v := range data {
+		fmt.Printf("Halo nama saya %s, umur saya %d tahun, dan alamat saya di %s", v.Nama, v.Umur, v.Alamat)
+	}
 }
